3d7185605afb0b2e2cc01b7c48a73b6fbc639030
Kelvin Vaz <118559500+kelvinvass22@users.noreply.github.com>
2025-08-25 23:36:37 -0300

    Delete modulo 2/atividade 1 directory

--- modulo 2/atividade 1/atividade4.js
+++
@@ -1,9 +0,0 @@
-let clubes = ["Flamengo", "Palmeiras", "Corinthians", "São Paulo", "Santos"];
-
-console.log("Times:", clubes);
-console.log("Quantidade de times:", clubes.length);
-
-clubes.push("Grêmio", "Internacional");
-
-console.log("Nova lista de times:", clubes);
-console.log("Novo total de times:", clubes.length);
