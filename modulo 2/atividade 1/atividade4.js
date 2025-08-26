644250ced3412c5f0e2c000d40574bbcf6d69f32
Kelvin Vaz <118559500+kelvinvass22@users.noreply.github.com>
2025-08-25 23:34:37 -0300

    Add files via upload

---
+++ modulo 2/atividade 1/atividade4.js
@@ -0,0 +1,9 @@
+let clubes = ["Flamengo", "Palmeiras", "Corinthians", "São Paulo", "Santos"];
+
+console.log("Times:", clubes);
+console.log("Quantidade de times:", clubes.length);
+
+clubes.push("Grêmio", "Internacional");
+
+console.log("Nova lista de times:", clubes);
+console.log("Novo total de times:", clubes.length);
